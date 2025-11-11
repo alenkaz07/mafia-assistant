d3a523840669cb1f3cccf667d9fee319e411511e
alenkaz07 <alenkaz07@MacBook-Pro-Alena.local>
2025-11-11 19:32:37 +0300

    urls added

--- README.md
+++ README.md
@@ -17,3 +17,13 @@ python manage.py runserver
 ![](./img/1.png)
 
 >http://127.0.0.1:8000/
+
+``` bash
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver
+```
+
+![](./img/2.png)
+
+>http://127.0.0.1:8000/admin
--- game/views.py
+++ game/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Sessions, Results
 
 # Create your views here.
+
+def index(request):
+    sessions_count = Sessions.objects.count()
+    mafia_wins = Results.objects.filter(winner_side='mafia').count()
+    town_wins = Results.objects.filter(winner_side='town').count()
+
+    context = {
+        'sessions_count': sessions_count,
+        'mafia_wins': mafia_wins,
+        'town_wins': town_wins,
+    }
+    return render(request, 'game/index.html', context)
