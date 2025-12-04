8bfc58a68f0950fc12b2e7ea8bb9532cc6eee0ee
alenkaz07 <alenkaz07@MacBook-Pro-Alena.local>
2025-12-04 19:39:48 +0300

    refactoring

--- mafia_assistant/settings.py
+++ mafia_assistant/settings.py
@@ -25,12 +25,6 @@ load_dotenv(BASE_DIR / ".env")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-# DEBUG = False
-
-# ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-
 DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
 if DEBUG:
